8ec3a26df392da66646581a090838100bb8506b5
o2plusdev <database.o2plus@gmail.com>
2021-04-01 20:36:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -508,7 +508,7 @@ app.post('/grimlim', urlencodedParser, function(req, res) {
 
 app.get('/divket', function(req, res) {
     var sess = req.session;
-    const fileStream = new_reg_bot.getFileStream("BAACAgUAAxkBAAMGYGXeCgPh-FiwG4fTWl8nntuvJi8AAjgCAAI-FTBX78j3X__ThsUeBA");
+    const fileStream = new_reg_bot.getFileStream("BAACAgUAAxkBAAMJYGXhL8t5Df8_UB177ESLVpBdr0IAAkECAAI-FTBXxWK6jUooSoUeBA");
     res.sendSeekable(fileStream, {
         type: 'video/mp4', // e.g. 'audio/mp4'
         length: '1585281',
